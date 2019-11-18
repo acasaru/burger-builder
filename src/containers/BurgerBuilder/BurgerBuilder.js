b37f9f3d42472e6adc230a04281467476fd8605b
Andreea Casaru <andreea.casaru@collinsongroup.com>
2019-11-18 17:08:46 +0200

    first commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
+
+
+class BurgerBuilder extends React.Component{
+    render(){
+        return(
+          <Auxiliary>
+              <div>buger display</div>
+              <div>burger controls</div>
+          </Auxiliary> 
+        );
+    }
+}
+
+export default BurgerBuilder
